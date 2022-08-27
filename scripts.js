04dacc6d7993aac09cc3b819a992c2d787684c69
billgx <billgx@ksu.edu>
2022-08-27 05:39:27 -0500

    completed example

--- scripts.js
+++ scripts.js
@@ -1,39 +1,5 @@
+// Openweathermap API. Do not share it publicly.
 const api = 'a89a7a5b22c0cf17ff5fecf41412c4d0';
-
-window.addEventListener('load',() => {
-    let long;
-    let lat;
-    // Accessing Geolocation of User
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            // Storing lat & long data in variables
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            const base = `https://api.openweathermap.org/data/2.5/weatherlat=${lat}&lon=${long}&appid=${api}&units=metric`;
-            console.log(base);
-            
-            // use fetch to get data from online api
-            fetch(base)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    const { temp } = data.main;
-                    const place = data.name;
-                    const { description, icon } = data.weather[0];
-                    const { sunrise, sunset } = data.sys;
-                    
-                    const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-                    const fahrenheit = (temp * 9) / 5 + 32;
-
-                    // converting Epoch(Unix) time to GMT
-                    const sunriseGMT = new Date(sunrise * 1000);
-                    const sunsetGMT = new Date(sunset * 1000);
-
-                });
-        });
-    }
-});
 
 const iconImg = document.getElementById('weather-icon');
 const loc = document.querySelector('#location');
@@ -43,12 +9,44 @@ const desc = document.querySelector('.desc');
 const sunriseDOM = document.querySelector('.sunrise');
 const sunsetDOM = document.querySelector('.sunset');
 
-iconImg.src = iconUrl;
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  // Accesing Geolocation of User
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // Storing Longitude and Latitude in variables
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
 
-loc.textContent = `${place}`;
-desc.textContent = `${description}`;
-tempC.textContent = `${temp.toFixed(2)} °C`;
-tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
-sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+      // Using fetch to get data
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { temp } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather[0];
+          const { sunrise, sunset } = data.sys;
 
+          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+          const fahrenheit = (temp * 9) / 5 + 32;
+
+          // Converting Epoch(Unix) time to GMT
+          const sunriseGMT = new Date(sunrise * 1000);
+          const sunsetGMT = new Date(sunset * 1000);
+
+          // Interacting with DOM to show data
+          iconImg.src = iconUrl;
+          loc.textContent = `${place}`;
+          desc.textContent = `${description}`;
+          tempC.textContent = `${temp.toFixed(2)} °C`;
+          tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+          sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+          sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+        });
+    });
+  }
+});
