aa387faa3357a2193555c47e62ceaf31681baf91
billgx <billgx@ksu.edu>
2022-08-27 05:35:42 -0500

    added DOM stuff to make the project work

--- scripts.js
+++ scripts.js
@@ -6,8 +6,49 @@ window.addEventListener('load',() => {
     // Accessing Geolocation of User
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position);
+            // Storing lat & long data in variables
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const base = `https://api.openweathermap.org/data/2.5/weatherlat=${lat}&lon=${long}&appid=${api}&units=metric`;
+            console.log(base);
+            
+            // use fetch to get data from online api
+            fetch(base)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    const { temp } = data.main;
+                    const place = data.name;
+                    const { description, icon } = data.weather[0];
+                    const { sunrise, sunset } = data.sys;
+                    
+                    const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+                    const fahrenheit = (temp * 9) / 5 + 32;
+
+                    // converting Epoch(Unix) time to GMT
+                    const sunriseGMT = new Date(sunrise * 1000);
+                    const sunsetGMT = new Date(sunset * 1000);
+
+                });
         });
     }
 });
+
+const iconImg = document.getElementById('weather-icon');
+const loc = document.querySelector('#location');
+const tempC = document.querySelector('.c');
+const tempF = document.querySelector('.f');
+const desc = document.querySelector('.desc');
+const sunriseDOM = document.querySelector('.sunrise');
+const sunsetDOM = document.querySelector('.sunset');
+
+iconImg.src = iconUrl;
+
+loc.textContent = `${place}`;
+desc.textContent = `${description}`;
+tempC.textContent = `${temp.toFixed(2)} °C`;
+tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
 
